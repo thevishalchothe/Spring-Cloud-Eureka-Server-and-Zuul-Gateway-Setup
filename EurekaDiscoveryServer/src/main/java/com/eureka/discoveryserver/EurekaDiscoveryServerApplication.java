0a9f3d94aefb8681793c8b1e9ee7315b47cf2077
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2024-12-27 13:44:57 +0530

    Created Spring Cloud Eureka Discovery Server 🌟🛠️

---
+++ EurekaDiscoveryServer/src/main/java/com/eureka/discoveryserver/EurekaDiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package com.eureka.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(EurekaDiscoveryServerApplication.class, args);
+		
+	}
+
+}
