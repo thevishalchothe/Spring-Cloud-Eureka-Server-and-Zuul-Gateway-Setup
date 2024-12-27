48029794ab486f862e22789543306681a7a6c132
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2024-12-27 14:01:38 +0530

    Eureka Discovery Server setup completed successfully 🌟🛠️

--- EurekaDiscoveryServer/src/main/java/com/eureka/discoveryserver/EurekaDiscoveryServerApplication.java
+++ EurekaDiscoveryServer/src/main/java/com/eureka/discoveryserver/EurekaDiscoveryServerApplication.java
@@ -2,14 +2,21 @@ package com.eureka.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer  // Enabling Eureka Server functionality
 public class EurekaDiscoveryServerApplication {
 
 	public static void main(String[] args) {
-		
-		SpringApplication.run(EurekaDiscoveryServerApplication.class, args);
-		
+		  // Print a message before running the application
+        System.out.println("Starting Eureka Discovery Server...");
+
+        // Run the Spring Boot application
+        SpringApplication.run(EurekaDiscoveryServerApplication.class, args);
+        
+        // Print a message after the application starts
+        System.out.println("Eureka Discovery Server is running!");
 	}
 
 }
