3ce29596169a1b9910bc32944e774536b91a7e05
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2024-12-29 13:25:57 +0530

    Zuul API Gateway setup completed successfully 🌟🛠️

--- ZuulAPIGatewayServer/src/main/java/com/zuulapi/getwayserver/ZuulApiGatewayServerApplication.java
+++ ZuulAPIGatewayServer/src/main/java/com/zuulapi/getwayserver/ZuulApiGatewayServerApplication.java
@@ -1,13 +1,27 @@
 package com.zuulapi.getwayserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class ZuulApiGatewayServerApplication {
 
 	public static void main(String[] args) {
+		
+	    final Logger logger = LoggerFactory.getLogger(ZuulApiGatewayServerApplication.class);
+        
+	    System.out.println("Zuul API Gateway Server is getting Started!");
+
 		SpringApplication.run(ZuulApiGatewayServerApplication.class, args);
+		
+        logger.info("Zuul API Gateway Server is up and running!");
+
 	}
 
 }
